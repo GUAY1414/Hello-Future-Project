80a1d98ce5e691c47bbf3e66587a2131f4960083
guay9700@gmail.com <guay9700@gmail.com>
2025-07-14 22:33:26 +0800

    sample

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) header("Location: login.php");
+include 'config/db.php';
+
+$id = $_SESSION['user_id'];
+$res = $conn->query("SELECT * FROM memories WHERE user_id=$id ORDER BY created_at DESC");
+
+echo "<a href='create.php'>Add New Memory</a> | <a href='logout.php'>Logout</a><hr>";
+
+while ($row = $res->fetch_assoc()) {
+    echo "<h3>{$row['title']}</h3>";
+    echo "<p><b>To be opened on:</b> {$row['unlock_date']}</p>";
+    echo "<p>" . ($row['unlock_date'] > date('Y-m-d') ? "<i>Locked</i>" : $row['content']) . "</p>";
+    if ($row['file']) {
+        echo "<a href='uploads/{$row['file']}'>View Attachment</a><br>";
+    }
+    echo "<a href='edit.php?id={$row['id']}'>Edit</a> | <a href='delete.php?id={$row['id']}'>Delete</a>";
+    echo "<hr>";
+}
+?>
