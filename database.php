111f180b8d8ef58e31ea533246aa39944cd58c04
Jericho <jericho.santos1620@gmail.com>
2025-07-16 22:08:51 +0800

    database

--- database.php
+++ database.php
@@ -8,5 +8,10 @@
 
     $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
-    if($conn)
+    if($conn){
+        echo "You are connected!";
+    }
+    else{
+        echo"Could not connect!";
+    }
 ?>
